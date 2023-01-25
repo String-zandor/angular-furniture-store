855bce88b93a0e283bf4da342cfa3eac2a775bee
maciecret <mercypulido4@gmail.com>
2023-01-25 14:56:16 +0800

    category and price

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 
 import { MatBadgeModule } from '@angular/material/badge';
-
-
-
 
 
 
@@ -20,6 +18,7 @@ import { MatBadgeModule } from '@angular/material/badge';
   imports: [
     CommonModule,
     MatBadgeModule,
+    
     
     
   ],
