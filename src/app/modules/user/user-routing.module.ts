75edb711449ba95412d5218daf6b6413f556abd2
String-zandor <zwritescode@gmail.com>
2023-01-27 22:16:42 +0800

    created user-auth guard
    -prevents access to cart, and profile unless logged in

--- src/app/modules/user/user-routing.module.ts
+++ src/app/modules/user/user-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserAuthGuard } from 'src/app/core/guards/user-auth.guard';
 import { ForgotPasswordComponent } from './pages/forgot-password/forgot-password.component';
 import { LoginComponent } from './pages/login/login.component';
 import { ProfileComponent } from './pages/profile/profile.component';
@@ -10,7 +11,11 @@ const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'forgot-password', component: ForgotPasswordComponent },
   { path: 'reset-password', component: ResetPasswordComponent },
-  { path: '', component: ProfileComponent }
+  { 
+    path: 'orders', canActivate: [UserAuthGuard], 
+    loadChildren: () => import('src/app/modules/order/order.module').then(m => m.OrderModule) 
+  },
+  { path: '', canActivate: [UserAuthGuard], component: ProfileComponent }
 ];
 
 @NgModule({
