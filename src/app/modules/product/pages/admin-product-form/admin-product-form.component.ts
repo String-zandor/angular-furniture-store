4c45c65d7e0ce0562841c467d2453e3ca2169611
String-zandor <zwritescode@gmail.com>
2023-02-01 21:06:33 +0800

    added autocomplete on room and category input

--- src/app/modules/product/pages/admin-product-form/admin-product-form.component.ts
+++ src/app/modules/product/pages/admin-product-form/admin-product-form.component.ts
@@ -2,7 +2,7 @@ import { Location } from '@angular/common';
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
-import { Subscription } from 'rxjs';
+import { distinct, from, map, Observable, Subscription, switchMap, toArray } from 'rxjs';
 import { ImageService } from 'src/app/core/services/image.service';
 import { DialogData } from 'src/app/shared/models/dialog-data';
 import { DialogService } from 'src/app/shared/services/dialog.service';
@@ -20,6 +20,7 @@ export class AdminProductFormComponent implements OnInit, OnDestroy {
   productForm: FormGroup = this.fb.group({
     id: [0],
     name: ['', Validators.required],
+    room: ['', Validators.required],
     category: ['', Validators.required],
     color: ['', Validators.required],
     description: this.fb.group({
@@ -30,6 +31,9 @@ export class AdminProductFormComponent implements OnInit, OnDestroy {
     }),
     price: [0, Validators.min(1)]
   });
+  allProducts$?: Observable<Product[]>;
+  roomOptions$?: Observable<string[]>;
+  categOptions$?: Observable<string[]>;
 
   selectedProd?: Product;
   photos?: FileList;
@@ -57,6 +61,27 @@ export class AdminProductFormComponent implements OnInit, OnDestroy {
         }
       });
     }
+    this.allProducts$ = this.productSvc.getProducts();
+    this.getRooms();
+    this.getCategories();
+  }
+
+  getCategories(): void {
+    this.categOptions$ = this.allProducts$?.pipe(
+      map(products => products.map(product => product.category)),
+      switchMap(categories => from(categories)),
+      distinct(),
+      toArray()
+    );
+  }
+
+  getRooms(): void {
+    this.roomOptions$ = this.allProducts$?.pipe(
+      map(products => products.map(product => product.room)),
+      switchMap(rooms => from(rooms)),
+      distinct(),
+      toArray()
+    );
   }
 
   updateForm(product: Product) {
@@ -107,21 +132,23 @@ export class AdminProductFormComponent implements OnInit, OnDestroy {
   updateProduct(): Product {
     const product: Product = this.productForm.value;
     product.postingDate = new Date().toJSON();
-    product.srcUrl = this.photosUrl;
+    if (this.photosUrl.length > 0) {
+      product.srcUrl = this.photosUrl;
+    }
     return product;
   }
 
   onSubmit(): void {
     const data: DialogData = {
       title: 'Confirm',
-      content: (this.selectedProd) 
+      content: (this.selectedProd)
         ? 'Would you like to save the changes to this product?'
         : 'Would you like to add this product?',
       confirm: 'Confirm',
       cancel: 'Cancel'
     };
     this.dialogSvc.confirm(data).subscribe(confirmed => {
-      if (confirmed){
+      if (confirmed) {
         this.saveProduct();
       }
     });
@@ -146,6 +173,10 @@ export class AdminProductFormComponent implements OnInit, OnDestroy {
 
   get name(): FormControl {
     return this.productForm.get('name') as FormControl;
+  }
+
+  get room(): FormControl {
+    return this.productForm.get('room') as FormControl;
   }
 
   get category(): FormControl {
